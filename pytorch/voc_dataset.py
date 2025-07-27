70d38cb256ded73d9168bdb6bfbf5b4adc4b4e17
Kallen6669 <18618151290@163.com>
2025-07-27 19:52:57 +0800

    dataset

--- pytorch/voc_dataset.py
+++ pytorch/voc_dataset.py
@@ -1,10 +1,13 @@
 from torch.utils.data import Dataset, DataLoader
+import torch
+from torchvision import transforms
 from PIL import Image
 import os
+import xmltodict
 
 
 class VOCDataset(Dataset):
-    def __init__(self, image_dir, label_dir, transform=None, label_transform=None, is_test=False):
+    def __init__(self, image_dir, label_dir,class_dir, transform=None, label_transform=None, is_test=False):
         super().__init__()
         self.image_dir = image_dir
         self.label_dir = label_dir
@@ -13,6 +16,8 @@ class VOCDataset(Dataset):
         self.is_test = is_test
         self.image_list = os.listdir(image_dir)
         self.label_list = os.listdir(label_dir)
+        with open(class_dir, "r") as f:
+            self.classes = f.read().splitlines()
         
     def __len__(self):
         return len(self.image_list)
@@ -21,16 +26,35 @@ class VOCDataset(Dataset):
         image_path = os.path.join(self.image_dir, self.image_list[idx])
         label_path = os.path.join(self.label_dir, self.label_list[idx])
         image = Image.open(image_path).convert("RGB")
-        label = Image.open(label_path).convert("L")
+        if self.transform is not None:
+            image = self.transform(image)
+        with open(label_path, "r") as f:
+            label_dict = xmltodict.parse(f.read())
+        label = [];
+        label_signal = []
+        for obj in label_dict["annotation"]["object"]:
+            label_signal = []
+            label_signal.append(self.classes.index(obj["name"]))
+            label_signal.append(obj["bndbox"]["xmin"])
+            label_signal.append(obj["bndbox"]["ymin"])
+            label_signal.append(obj["bndbox"]["xmax"])
+            label_signal.append(obj["bndbox"]["ymax"])
+            label.append(label_signal)
         return image, label
 
 
 def main():
-    image_dir = "/voc_data/train/imgs"
-    label_dir = "/voc_data/train/labels"
-    dataset = VOCDataset(image_dir, label_dir)
-    print(len(dataset))
-    print(dataset[0])
+    image_dir = "voc_data/train/imgs"
+    label_dir = "voc_data/train/labels"
+    class_dir = "voc_data/train/classes"
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    dataset = VOCDataset(image_dir, label_dir, class_dir, transform=transform)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=4)
+    for idx, (images, labels) in enumerate(dataloader):
+        print(images.shape)
+        print(labels)
 
 
 if __name__ == "__main__":
